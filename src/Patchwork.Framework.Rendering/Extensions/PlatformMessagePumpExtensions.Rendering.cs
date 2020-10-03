f2d07997e8ac5805e5439324b686f7ffd23b0203
Bryan Longacre <bry.psi@gmail.com>
2020-10-03 03:47:25 -0400

    Create PlatformMessagePumpExtensions.Rendering.cs

---
+++ src/Patchwork.Framework.Rendering/Extensions/PlatformMessagePumpExtensions.Rendering.cs
@@ -0,0 +1,44 @@
+﻿#region Usings
+using System.Drawing;
+using Hatzap.Rendering;
+using Patchwork.Framework.Messaging;
+using Patchwork.Framework.Platform.Rendering;
+using Patchwork.Framework.Platform.Windowing;
+#endregion
+
+namespace Patchwork.Framework.Extensions
+{
+    public static partial class PlatformMessagePumpExtensions
+    {
+        #region Methods
+        public static void PushRenderMessage(this IPlatformMessagePump pump, RenderMessageIds messageId, INRenderer renderer)
+        {
+            pump.Push(new PlatformMessage(MessageIds.Rendering, new RenderMessageData(messageId, renderer)));
+        }
+
+        public static void PushRenderMessage(this IPlatformMessagePump pump, IRenderMessageData data)
+        {
+            pump.Push(new PlatformMessage(MessageIds.Rendering, data));
+        }
+
+        public static void PushRenderOsMessage(this IPlatformMessagePump pump,
+                                               INWindow window,
+                                               Point requestedPosition)
+        {
+
+        }
+
+        public static void PushFrameBuffer(this IPlatformMessagePump pump, INRenderer renderer, NFrameBuffer frameBuffer)
+        {
+            pump.PushRenderMessage(new RenderMessageData(renderer).SetFrameBuffer(frameBuffer));
+        }
+
+        public static void PushRenderWindowMessage(this IPlatformMessagePump pump,
+                                                   INWindow window,
+                                                   Point requestedPosition)
+        {
+            //pump.PushWindowMessage(new RenderMessageData(window));
+        }
+        #endregion
+    }
+}
